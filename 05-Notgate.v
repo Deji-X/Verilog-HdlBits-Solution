c01fa7fd7cbc3b32242e70410627098936176f3f
Huh.... <henry6769wisdom@gmail.com>
2025-04-11 01:05:00 +0100

    Create 05-Notgate.v

---
+++ 05-Notgate.v
@@ -0,0 +1,6 @@
+module top_module( input in, output out);
+  assign out = ~in;
+  // ~ is the bitwise function for "NOT"
+  // | is the bitwise function for "OR"
+  // & is the bitwise function for "AND"
+endmodule
