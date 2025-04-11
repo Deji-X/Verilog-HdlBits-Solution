88d26b5440424e522ada42d0992afd38155b8ab6
Huh.... <henry6769wisdom@gmail.com>
2025-04-11 19:48:04 +0100

    Update 4- VectorGates.v

--- 2- Vectors/4- VectorGates.v
+++ 2- Vectors/4- VectorGates.v
@@ -1,7 +1,11 @@
 module top_module(
-  input  [2:0] a, b,
-  output [2:0] out_or_bitwise, // Bitwise OR = |
-  output out_or_logical, // Logical OR = ||
-  output [5:0] out_not, // Bitwise NOT = ~
+  input  [2:0] a, b,                // Since a and b have the same number of bits, created them simulateanously.
+  output [2:0] out_or_bitwise,      // Bitwise OR = |
+  output out_or_logical,            // Logical OR = ||
+  output [5:0] out_not,             // Bitwise NOT = ~
 );
-  
+  assign out_or_bitwise = a | b;
+  assign out_or_logical = a || b;
+  assign out_not[5:3] = ~b;
+  assign out_not[2:0] = ~a;
+endmodule
