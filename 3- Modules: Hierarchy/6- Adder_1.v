8529775674780c273dd2bb191cfc5360eb7b7ef8
Huh.... <henry6769wisdom@gmail.com>
2025-04-14 00:51:36 +0100

    Update 6- Adder_1.v

--- 3- Modules: Hierarchy/6- Adder_1.v
+++ 3- Modules: Hierarchy/6- Adder_1.v
@@ -4,9 +4,8 @@ module top_module(
   output [31:0] sum
 );
 
-  wire cout1;
-  wire cout2;
-
+  wire cout1, cout2;
+  
   add16 add1( a[15:0], b[15:0], 0, sum[15:0], cout1);
   add16 add2( a[31:16], b[31:16], cout1, sum[31:16], cout2);
 
