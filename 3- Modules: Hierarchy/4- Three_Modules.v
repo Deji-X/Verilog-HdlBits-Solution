bb8aface405879f8bbace509c3c9a4886fc75f62
Huh.... <henry6769wisdom@gmail.com>
2025-04-13 03:33:58 +0100

    Update 4- Three_Modules.v

--- 3- Modules: Hierarchy/4- Three_Modules.v
+++ 3- Modules: Hierarchy/4- Three_Modules.v
@@ -1,25 +1,20 @@
-module top_module(
-  input clk,
-  input d,
-  output q
-);
+module top_module( input clk, input d, output q);
   wire q1;
   wire q2;
 
   my_dff ff1(
     .clk(clk),
     .d(d),
-    .q(q1)
-  );
-  my_diff ff2(
+    .q(q1));
+  
+  my_dff ff2(
     .clk(clk),
     .d(q1),
-    .q(q2)
-  );
-  my_diff ff3(
+    .q(q2));
+  
+  my_dff ff3(
     .clk(clk),
     .d(q2),
-    .q(q)
-  );
+    .q(q));
 
 endmodule
