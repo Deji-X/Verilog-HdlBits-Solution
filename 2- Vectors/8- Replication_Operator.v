455630b82c069796e48e325448fb2770c5c46878
Huh.... <henry6769wisdom@gmail.com>
2025-04-13 01:19:06 +0100

    Create 8- Replication_Operator.v

---
+++ 2- Vectors/8- Replication_Operator.v
@@ -0,0 +1,14 @@
+module top_module(
+  input [7:0] in,
+  output [31:0] out,
+);
+  
+  
+  assign out = { {24 {in[7]}} , in[7:0]};
+  /* concatenation of 24 copies of the sign bit (i.e., replicate bit[7] 24 times) followed by the 8-bit number itself.
+     So the first bit says concatenate 24 copies of the 8 or MSB of the in, then the second just describes the 8-bit number
+  */
+
+endmodule
+
+  
