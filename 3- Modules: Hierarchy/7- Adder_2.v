6b443efb7364e3a7feb3919d24e0184c889712b5
Huh.... <henry6769wisdom@gmail.com>
2025-04-14 16:20:59 +0100

    Update 7- Adder_2.v

--- 3- Modules: Hierarchy/7- Adder_2.v
+++ 3- Modules: Hierarchy/7- Adder_2.v
@@ -1,17 +1,16 @@
-module top_module(
-  input [31:0] a,
-  input [31:0] b,
-  output [31:0] sum
-);
-
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);//
+  
   wire cout1, cout2;
 
   add16 add_1(a[15:0], b[15:0], 0, sum[15:0], cout1);
   add16 add_2(a[31:16], b[31:16], cout1, sum[31:16], cout2);
-
 endmodule
 
-module add1 ( input a, input b, input cin, output sum, output cout );
+module add1 ( input a, input b, input cin,   output sum, output cout );
 
   assign sum = a ^ b ^ cin;
   // The sum formula of a full adder is a xor b xor cin
