3f00731b7e787723b6088c240ad7d5d331ccdc64
Huh.... <henry6769wisdom@gmail.com>
2025-04-15 00:38:01 +0100

    Update 9- Adder_Subtractor.v

--- 3- Modules: Hierarchy/9- Adder_Subtractor.v
+++ 3- Modules: Hierarchy/9- Adder_Subtractor.v
@@ -5,11 +5,11 @@ module top_module(
   output [31:0] sum
 );
   
-  wire [15:0] bsub;
+  wire [31:0] bsub;
   wire [15:0] cout1;
   wire [15:0] cout2;
-  
-  assign bsub = b ^ sub;
+
+  assign bsub = {32{sub}} ^ b;
   
   add16 add1(a[15:0], sub[15:0], sub, sum[15:0], cout1);
   add16 add1(a[31:16], sub[31:16], cout1, sum[31:16], cout2);
