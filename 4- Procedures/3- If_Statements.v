c34898d83b43b16f5f09f7f24a7e34c8a9efc8ed
Huh.... <henry6769wisdom@gmail.com>
2025-04-16 02:44:37 +0100

    Update 3- If_Statements.v

--- 4- Procedures/3- If_Statements.v
+++ 4- Procedures/3- If_Statements.v
@@ -7,15 +7,17 @@ module top_module(
     output wire out_assign,
     output reg out_always   );
 
-  /*1'b1 = 1 bit binary value of 1(1)
-  */
-  assign out_assign = (sel_b1=1'b1 & sel_b2= 1'b1) ? b:a;
+  //1'b1 = 1 bit binary value of 1(1)
   
-  //else assign out_assign = a; 
+    assign out_assign = (sel_b1==1'b1 & sel_b2==1'b1) ? b:a;
+  
+    /* b:a - b first as the output if the statement is true
+        - a second, as an output if the first statement is false */
+    
   //My little idea, might just be poppycock or bollocks!!
 
     always @(*) begin
-    if (sel_b=1'b1 & sel_b2= 1'b1) begin
+        if (sel_b==1'b1 & sel_b2==1'b1) begin
       out_always = b;
     end
     else begin
