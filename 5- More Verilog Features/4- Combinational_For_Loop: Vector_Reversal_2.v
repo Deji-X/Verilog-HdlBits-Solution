16dd0e37907762fe60a3a22cac941b6fa3a77b60
Huh.... <henry6769wisdom@gmail.com>
2025-04-18 23:48:30 +0100

    Create 4- Combinational_For_Loop: Vector_Reversal_2.v

---
+++ 5- More Verilog Features/4- Combinational_For_Loop: Vector_Reversal_2.v
@@ -0,0 +1,13 @@
+module top_module(
+  input  [99:0]  in,
+  output [99:0] out,
+);
+/* integer w;
+    always @ (in) begin 
+        for (w=0; w<100; w=w+1) begin
+            out[99-w] = in[w];
+        end        
+    end
+*/  
+
+endmodule
