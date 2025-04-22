ecb919c55c1960ce8c9b0e7b2903088e8137ee41
Huh.... <henry6769wisdom@gmail.com>
2025-04-22 02:10:46 +0100

    Update 2. 4-Variable.v

--- Circuits/Combinational Logic/4- Karnaugh_Map_To_Circuit/2. 4-Variable.v
+++ Circuits/Combinational Logic/4- Karnaugh_Map_To_Circuit/2. 4-Variable.v
@@ -6,6 +6,12 @@ module top_module(
   output out
 );
 
-  assign out = ;
+  /* All my reduction techniques and equations are in my personal note, you can email
+  for it (henry6769wisdom@gmail.com) used a hybrid of MCCLUSKEY algorithm and equation
+  in something I call Wisdom's correctional proof, do check the main readme, I might
+  have completed the fullnote and left a drive link....*/
+
+  assign out = (~a&~d) | (~b&~c) | (~a&b&c) | (a&c&d); //SOP
+  //assign out = (~a|~b|c|d) & (~a|b|~c|d) & (a|~b|c|~d) & (a|b|~c|~d) & (a|b|~c|d) & (a|b|c|~d);
 
 endmodule
