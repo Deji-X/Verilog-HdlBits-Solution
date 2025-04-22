15332a7c698dcef0c20d8a41641be8f928bb70e6
Huh.... <henry6769wisdom@gmail.com>
2025-04-22 20:33:13 +0100

    Create 3. 4-Variable.v

---
+++ Circuits/Combinational Logic/4- Karnaugh_Map_To_Circuit/3. 4-Variable.v
@@ -0,0 +1,12 @@
+module top_module(
+  input a,
+  input b,
+  input c,
+  input d,
+  output out
+);
+// For Do not care situations, only rule, do not have cells filled with DNC
+// conditions as the odd of all happening at the same time is extremely rare...
+
+  assign out =
+  
