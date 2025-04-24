7ec6edc7155728975fe16124b2d9cabcfc9369d5
Huh.... <henry6769wisdom@gmail.com>
2025-04-24 01:50:45 +0100

    Create 8. DFF_With_Asynchronous_Reset.v

---
+++ Circuits/Sequential Logic/1. Latches And Flip-Flops/8. DFF_With_Asynchronous_Reset.v
@@ -0,0 +1,24 @@
+module top_module(
+  input clk,
+  input areset,
+  input [7:0] d,
+  output [7:0] q
+);
+  
+  always @ (posedge clk or posedge areset) begin
+    if (areset)
+      q <= 0;
+    else
+      q <= d;
+  end
+
+  /* Their solution;
+  always @(posedge clk, posedge areset)
+		if (areset)
+			q <= 0;
+		else
+			q <= d;
+  */
+endmodule
+
+  
