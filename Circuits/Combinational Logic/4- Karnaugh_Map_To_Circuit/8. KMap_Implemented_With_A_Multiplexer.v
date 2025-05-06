523d06cad23bf295552a82cebf5af34641fc4c9a
Henry Wisdom <henry6769wisdom@gmail.com>
2025-05-07 00:45:11 +0100

    Update 8. KMap_Implemented_With_A_Multiplexer.v

--- Circuits/Combinational Logic/4- Karnaugh_Map_To_Circuit/8. KMap_Implemented_With_A_Multiplexer.v
+++ Circuits/Combinational Logic/4- Karnaugh_Map_To_Circuit/8. KMap_Implemented_With_A_Multiplexer.v
@@ -8,8 +8,13 @@ module top_module(
   a and b are select variables
   */
 
-  assign mux_in[0] = c ? 1 : d;
-  assign mux_in[1] = 0;
-  assign mux_in[2] = d ? 0 : 1;
-  assign mux_in[3] = c ? d : 0
+  // After splitting the truth table into four columns,
+	// the rest of this question involves implementing logic functions
+	// using only multiplexers (no other gates).
+	// I will use the conditional operator for each 2-to-1 mux: (s ? a : b)
+
+  assign mux_in[0] = c ? 1 : d;          // 1 mux:   c|d
+	assign mux_in[1] = 0;                  // No muxes:  0
+	assign mux_in[2] = d ? 0 : 1;          // 1 mux:    ~d
+	assign mux_in[3] = c ? d : 0;          // 1 mux:   c&d
 endmodule
