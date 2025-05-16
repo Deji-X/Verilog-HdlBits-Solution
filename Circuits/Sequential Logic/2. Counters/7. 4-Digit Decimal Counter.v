56eed2afa612cf1b50b00f96693ffa6a3733a8bc
Henry Wisdom <henry6769wisdom@gmail.com>
2025-05-16 15:10:19 +0100

    Create 7. 4-Digit Decimal Counter.v

---
+++ Circuits/Sequential Logic/2. Counters/7. 4-Digit Decimal Counter.v
@@ -0,0 +1,13 @@
+// Exams/ece241 2014 q7b
+
+module top_module(
+  input clk,
+  input reset,
+  output OneHertz,
+  output [2:0] c_enable
+);
+  
+  bcdcount counter0 (clk, reset, c_enable[0]/*, ... */);
+  bcdcount counter1 (clk, reset, c_enable[1]/*, ... */);
+
+endmodule
