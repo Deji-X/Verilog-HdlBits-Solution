3e728453ee755703f1fd819969b4fc2a764974d2
Henry Wisdom <henry6769wisdom@gmail.com>
2025-05-19 10:16:10 +0100

    Update 17. Even_Longer_Vectors.v

--- Circuits/Combinational Logic/Basic Gates/17. Even_Longer_Vectors.v
+++ Circuits/Combinational Logic/Basic Gates/17. Even_Longer_Vectors.v
@@ -6,11 +6,9 @@ module top_module(
   output [99:1] out_any,
   output [99:0] out_different
 );
-  
-  assign out_both = in[99:0] & in[98:1];
-  assign out_any = in[99:0] | in[98:1];
-  assign out_different = // here requires a concatenation..;
-    
+  assign out_both = in[98:0] & in[99:1];
+  assign out_any = in[99:1] | in[98:0];
+  assign out_different = in[99:0] ^ {in[0], in[99:1]};
     
 endmodule
     
