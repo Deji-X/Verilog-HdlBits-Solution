6b45b516136464fe4debdd494fd99f480758bab2
Henry Wisdom <henry6769wisdom@gmail.com>
2025-07-08 01:26:21 +0100

    Update 1. Mux.v

--- Verification: Reading Simulations/1. Finding_Bug_In_Code/1. Mux.v
+++ Verification: Reading Simulations/1. Finding_Bug_In_Code/1. Mux.v
@@ -2,9 +2,13 @@ module top_module(
   input sel,
   input [7:0] a,
   input [7:0] b,
-  output out
+  output [7:0] out
 );
 
-  assign out = (~sel & a) | (sel & b);
+  // assign out = ({8{~sel}} & a) | ({8{sel}} & b);
+  // this is a solution I found online...mine is below and is easier to understand.
+  // the above uses concantenation.
+
+  assign out = sel ? a : b;
 
 endmodule
