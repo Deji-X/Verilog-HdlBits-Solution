f7d4cd74672d9e7789b40e765afd9ca3334ece44
Henry Wisdom <henry6769wisdom@gmail.com>
2025-07-09 13:24:34 +0100

    Update 05. TFlip-Flop.v

--- Verification: Writing Testbenches/05. TFlip-Flop.v
+++ Verification: Writing Testbenches/05. TFlip-Flop.v
@@ -1,7 +1,20 @@
 //Tb/tff
 
-module top_module(
+module top_module();
+  reg clk, reset, t;
+  wire q;
+
+  tff inst(clk, reset, t, q);
+
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+
+  initial begin
+    t = 1; reset = 1;
+    #10 reset = 0;
+  end
   
-);
 
 endmodule
