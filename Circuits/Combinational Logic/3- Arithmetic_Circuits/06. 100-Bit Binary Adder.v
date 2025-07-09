5af9d5d7535b8b3cdbfb11ecd7ee21e9a779b30f
Henry Wisdom <henry6769wisdom@gmail.com>
2025-07-09 16:15:36 +0100

    Update 06. 100-Bit Binary Adder.v

--- Circuits/Combinational Logic/3- Arithmetic_Circuits/06. 100-Bit Binary Adder.v
+++ Circuits/Combinational Logic/3- Arithmetic_Circuits/06. 100-Bit Binary Adder.v
@@ -1,7 +1,16 @@
 // Adder100
 
 module top_module(
-  
+  input [99:0] a, b,
+  input cin,
+  output cout,
+  output [99:0] sum
 );
+
+  wire 
+  //This a 100 bit number, I am not going to write a function for 100 bits each,
+  //hence, genvar i...generate.
+
+  
 
 endmodule
