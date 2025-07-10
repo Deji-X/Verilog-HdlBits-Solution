a1b6e5e5dc49dfa73017b5f352a619ea234ce16d
Henry Wisdom <henry6769wisdom@gmail.com>
2025-07-10 12:22:43 +0100

    Update 15.EdgeDetect.v

--- Circuits/Sequential Logic/1. Latches And Flip-Flops/15.EdgeDetect.v
+++ Circuits/Sequential Logic/1. Latches And Flip-Flops/15.EdgeDetect.v
@@ -3,5 +3,11 @@ module top_module(
   input [7:0] in,
   input [7:0] pedge
 );
+  reg [7:0] not_pedge;
+
+  always @ (posedge clk) begin
+    not_pedge <= in;
+    pedge <= in & ~not_pedge;
+  end
 
 endmodule
