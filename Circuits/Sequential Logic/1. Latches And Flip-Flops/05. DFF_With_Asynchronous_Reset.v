5a911fd00d635dbf2ae779ccc8dc34f900f5b9ac
Henry Wisdom <henry6769wisdom@gmail.com>
2025-07-11 23:54:27 +0100

    Update 05. DFF_With_Asynchronous_Reset.v

--- Circuits/Sequential Logic/1. Latches And Flip-Flops/05. DFF_With_Asynchronous_Reset.v
+++ Circuits/Sequential Logic/1. Latches And Flip-Flops/05. DFF_With_Asynchronous_Reset.v
@@ -4,13 +4,13 @@ module top_module(
   input [7:0] d,
   output [7:0] q
 );
-  
-  always @ (posedge clk or posedge areset) begin
-    if (areset)
-      q <= 0;
-    else
-      q <= d;
-  end
+	
+	always @ (posedge clk or posedge areset) begin
+		if (areset)
+			q <= 0;
+		else
+		q <= d;
+	end
 
   /* Their solution;
   always @(posedge clk, posedge areset)
