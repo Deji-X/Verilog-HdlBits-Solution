55acf55b5a93b030b4454ee63913e5df1690fd71
Henry Wisdom <henry6769wisdom@gmail.com>
2025-07-12 01:15:04 +0100

    Update 12.MUXandDFF2(Exams2014q4a).v

--- Circuits/Sequential Logic/1. Latches And Flip-Flops/12.MUXandDFF2(Exams2014q4a).v
+++ Circuits/Sequential Logic/1. Latches And Flip-Flops/12.MUXandDFF2(Exams2014q4a).v
@@ -4,4 +4,15 @@ module top_module(
   output Q
 );
 
-  wire 
+  wire [1:0] huh;
+  assign huh = {E, L};
+  always @ (posedge clk) begin
+    case (huh)
+      2'b00 : Q <= Q;
+      2'b01 : Q <= R;
+      2'b10 : Q <= w;
+      2'b11 : Q <= R;
+    endcase
+  end
+
+endmodule
