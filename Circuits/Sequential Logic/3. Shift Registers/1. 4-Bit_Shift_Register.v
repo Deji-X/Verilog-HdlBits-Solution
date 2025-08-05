03e2020b7ea2e23ed084c9a924bc1582e8872d75
Henry Wisdom <henry6769wisdom@gmail.com>
2025-08-05 21:57:48 +0100

    Update 1. 4-Bit_Shift_Register.v

--- Circuits/Sequential Logic/3. Shift Registers/1. 4-Bit_Shift_Register.v
+++ Circuits/Sequential Logic/3. Shift Registers/1. 4-Bit_Shift_Register.v
@@ -1,5 +1,19 @@
 module top_module(
-  
+  input clk,
+  input areset, // async-high reset to zero
+  input load,
+  input ena,
+  input [3:0] data,
+  output reg [3:0] q
 );
+
+  always @(posedge clk or posedge areset) begin
+    if(areset)
+      q <= 0;
+    else if(load)
+      q <= data;
+    else
+      q <= ena ? q>>1 : q;
+  end
 
 endmodule
