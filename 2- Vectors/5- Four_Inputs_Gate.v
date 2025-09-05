ce0d10a6f55815e08546c0f0a2bb00f24907600a
Henry Wisdom <henry6769wisdom@gmail.com>
2025-09-05 20:43:43 +0100

    Update 5- Four_Inputs_Gate.v

--- 2- Vectors/5- Four_Inputs_Gate.v
+++ 2- Vectors/5- Four_Inputs_Gate.v
@@ -14,4 +14,12 @@ module top_module(
   assign out_or = |in;
   assign out_xor = ^in;
   */
+
+  /*
+  So this is another solution with concatenation, this comes at 05/09/2025
+  months after my inital code...
+  assign out_and = &{in[3], in[2], in[1], in[0]};
+  assign out_or  = |{in[3], in[2], in[1], in[0]};
+  assign out_xor = ^{in[3], in[2], in[1], in[0]};
+  */
 endmodule
