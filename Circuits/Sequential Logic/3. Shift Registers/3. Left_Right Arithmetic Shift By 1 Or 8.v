97e993588027d51ad306615249c2f99cb3916c94
Henry Wisdom <henry6769wisdom@gmail.com>
2026-08-31 02:23:42 +0100

    Implement left/right arithmetic shift logic

--- Circuits/Sequential Logic/3. Shift Registers/3. Left_Right Arithmetic Shift By 1 Or 8.v
+++ Circuits/Sequential Logic/3. Shift Registers/3. Left_Right Arithmetic Shift By 1 Or 8.v
@@ -9,6 +9,19 @@ module top_module(
   output reg [63:0] q
 );
 
-  
+  always @(posedge clk)begin
+        if(load) 
+            q<=data;
+        else begin
+            case(ena)
+                'b00: q<={q[0],q[99:1]};
+                'b01: q<={q[98:0],q[99]};
+                'b10: q<={q[98:0],q[99]};
+                'b11: q<={q[98:0],q[99]};
+                default: q<=q;
+            endcase
+        end
+    end
 
+  
 endmodule
