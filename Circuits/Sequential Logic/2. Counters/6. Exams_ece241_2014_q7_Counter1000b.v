769a053ac5c22214a66d61c7a8d1ba5d910d3dd9
Henry Wisdom <henry6769wisdom@gmail.com>
2026-08-31 01:08:59 +0100

    Update 6. Exams_ece241_2014_q7_Counter1000b.v

--- Circuits/Sequential Logic/2. Counters/6. Exams_ece241_2014_q7_Counter1000b.v
+++ Circuits/Sequential Logic/2. Counters/6. Exams_ece241_2014_q7_Counter1000b.v
@@ -7,10 +7,17 @@ module top_module(
   output [2:0] c_enable
 );
 
-  wire [3:0] w1, w2, w3;
+  wire [3:0] W1, W2, W3;
 
-  bcdcount counter0 (clk, reset, c_enable[0]);
-  bcdcount counter1 (clk, reset, c_enable[1]);
-  bcdcount counter2 (clk, reset, c_enable[2]);
+  assign c_enable[0] = 1'b1;
+  assign c_enable[1] = c_enable[0] && (W1 == 4'd9);
+  assign c_enable[2] = c_enable[1] && (W2 == 4'd9);
+
+  assign OneHertz = 1'b1 && (W1 == 4'd9) && (W2 == 4'd9) && (W3 == 4'd9);
+  // assign OneHertz = c_enable[2] && (W3 == 4'd9); 
+
+  bcdcount counter0 (clk, reset, c_enable[0], W1);
+  bcdcount counter1 (clk, reset, c_enable[1], W2);
+  bcdcount counter2 (clk, reset, c_enable[2], W3);
 
 endmodule
