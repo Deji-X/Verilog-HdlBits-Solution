c936b644eeaa4d73708e4dda6d735cce4ae21175
Henry Wisdom <henry6769wisdom@gmail.com>
2026-09-01 22:18:56 +0100

    Update 6. 32-bit LFSR.v

--- Circuits/Sequential Logic/3. Shift Registers/6. 32-bit LFSR.v
+++ Circuits/Sequential Logic/3. Shift Registers/6. 32-bit LFSR.v
@@ -5,16 +5,30 @@ module top_module(
 );
   always @(posedge clk) begin
     if (reset) begin
-      q <= q[31:1];
+      q <= 32'h1;
     end
     else begin
       if (q[0]) begin
-        q <= {q[0], q[31:1]} ^  32'h10000000001000000000000000000011;
+          q <= {q[0] ^ 1'b1, q[31:1]} ^ 32'h80200003;
       end
       else begin
-        q <= {q[0], q[31:1]};
+          q <= {q[0], q[31:1]};
       end
     end
   end
+  /*
+  always @(posedge clk) begin
+    if (reset) begin
+        q <= 32'h1;
+    end
+    else begin
+        if (q[0]) begin
+            q <= {1'b0, q[31:1]} ^ 32'h80200003;
+        end
+        else begin
+            q <= {1'b0, q[31:1]};
+        end
+    end
+end*/
 
 endmodule
