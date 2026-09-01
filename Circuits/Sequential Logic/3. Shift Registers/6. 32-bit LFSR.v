f3e1ee8db2ae38c66344fd6388480b4822a43f0a
Henry Wisdom <henry6769wisdom@gmail.com>
2026-09-01 19:28:22 +0100

    Add shift logic to 32-bit LFSR
    Implement LFSR shift operation on reset and update logic.

--- Circuits/Sequential Logic/3. Shift Registers/6. 32-bit LFSR.v
+++ Circuits/Sequential Logic/3. Shift Registers/6. 32-bit LFSR.v
@@ -3,5 +3,12 @@ module top_module(
   input reset,
   output [31:0] q
 );
+  always @(posedge clk) begin
+    if (reset) begin
+      q <= q[31:1];
+    end
+    else begin
+      if (q[0]) begin
+        q <= {} 
 
 endmodule
